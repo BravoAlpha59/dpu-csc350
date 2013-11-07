9fd2e2c95e287b0c71fb98d495d5db82c5d279d8
RT Arrington <robertarrington_2015@depauw.edu>
2013-11-07 12:44:18 -0500

    Successful compilation of hw5.cc and drawSphere.h drawing 2 spheres without glutSolidSphere using slices and stacks and geometry calculation and then adding light sources and some small abount of texturing.

---
+++ hw5/drawSphere.h
@@ -0,0 +1,148 @@
+//
+// File: drawSphere.h
+// Author: Douglas Harms
+//
+// Description: defines a function to draw a sphere
+//
+#include <GL/glut.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+//#define BA_DEBUG
+
+#ifdef BA_DEBUG
+#define Debug(x) cout << x
+#else
+#define Debug(x)
+#endif
+
+void drawSphere (int numSlices, int numStacks)
+{
+  // draws a unit sphere with the origin at the center.  The "top" of the
+  // sphere will be on the y-axis.  Normals will be defined appropriately.
+  GLdouble deltaTheta = (2.0 * M_PI)/numSlices ;  // radians
+  GLdouble deltaY = 2.0 / numStacks;
+  // stack offset
+  GLdouble * y = new GLdouble [numStacks+1];
+  // slice offset
+  GLdouble * r = new GLdouble [numStacks+1];
+  // quad vertices
+  GLdouble * leftX = new GLdouble [numStacks+1];
+  GLdouble * leftZ = new GLdouble [numStacks+1];
+  GLdouble * rightX = new GLdouble [numStacks+1];
+  GLdouble * rightZ = new GLdouble [numStacks+1];
+
+  // top of sphere on Y is at 1.0
+  y[0]=1.0;
+  // bottom of sphere on Y is at -1.0
+  y[numStacks]=-1.0;
+  // origin on X is 0.0
+  r[0]=0.0;
+  // last position on X is 0.0
+  r[numStacks]=0.0;
+
+  leftX[0] = r[0];
+  leftZ[0] = 0.0;
+
+  Debug("Stacks:" << numStacks << " Slices:" << numSlices << endl);
+  Debug("deltaTheta:" << deltaTheta << " radians " << deltaTheta * (180 / M_PI) << " degrees" << endl);
+  Debug("deltaY:" << deltaY  << endl);
+  Debug("----------" << endl);
+
+  Debug("0" << ": y=" << y[0] << " r=" << r[0] << endl);
+
+  int i, j;
+  for(i = 1; i < numStacks; i++) {
+      // calculate Y position of each stack
+      y[i]= y[i-1] - deltaY;
+      // sqrt of ( radius - Y*Y )
+      r[i]= sqrt(1.0 - pow(y[i], 2));
+      // initialize leftX = r
+      leftX[i] = r[i];
+      // initialize leftZ = 0.0
+      leftZ[i] = 0.0;
+
+      Debug(i << ": y=" << y[i] << " r=" << r[i] << endl);
+  }
+
+  Debug(numStacks << ": y=" << y[numStacks] << " r=" << r[numStacks] << endl);
+  Debug("++++++++++" << endl);
+
+  GLdouble theta = deltaTheta;
+
+  for(j = 0; j <= numSlices; j++) {
+
+      glBegin(GL_QUADS);
+
+      // close the top
+      glNormal3d(leftX[0] , y[0], leftZ[0]);
+      glVertex3d(leftX[0] , y[0], leftZ[0]);
+      glNormal3d(rightX[0] , y[0], rightZ[0]);
+      glVertex3d(rightX[0] , y[0], rightZ[0]);
+      glNormal3d(r[1] * cos(theta) , y[1], -r[1] * sin(theta));
+      glVertex3d(r[1] * cos(theta) , y[1], -r[1] * sin(theta));
+      glNormal3d(leftX[1] , y[1], leftZ[1]);
+      glVertex3d(leftX[1] , y[1], leftZ[1]);
+
+
+      // fill out the stacks
+      for(i = 1; i < numStacks; i++) {
+          Debug("stack: " << i  << " theta: " << theta << endl);
+
+          rightX[i] = r[i] * cos(theta);
+          rightX[i+1] = r[i+1] * cos(theta);
+
+          rightZ[i] = -r[i] * sin(theta);
+          rightZ[i+1] = -r[i+1] * sin(theta);
+
+          // top-left
+          Debug("TL->" << "leftX: " << leftX[i] << "  y: " << y[i] << "  leftZ: " << leftZ[i] << endl);
+          glNormal3d(leftX[i] , y[i], leftZ[i]);
+          glVertex3d(leftX[i] , y[i], leftZ[i]);
+
+          // top-right
+          Debug("TR->" << "rightX: " << rightX[i] << "  y: " << y[i] << "  rightZ: " << rightZ[i] << endl);
+          glNormal3d(rightX[i] , y[i], rightZ[i]);
+          glVertex3d(rightX[i] , y[i], rightZ[i]);
+
+          // bottom-right
+          Debug("BR->" << "rightX: " << rightX[i+1] << "  y: " << y[i+1] << "  rightZ: " << rightZ[i+1] << endl);
+          glNormal3d(rightX[i+1] , y[i+1], rightZ[i+1]);
+          glVertex3d(rightX[i+1] , y[i+1], rightZ[i+1]);
+
+          // bottom-left
+          Debug("BL->" << "  leftX: " << leftX[i+1] << "  y: " << y[i+1] << "  leftZ: " << leftZ[i+1] << endl);
+          glNormal3d(leftX[i+1] , y[i+1], leftZ[i+1]);
+          glVertex3d(leftX[i+1] , y[i+1], leftZ[i+1]);
+
+          leftX[i] = rightX[i];
+          leftZ[i] = rightZ[i];
+
+          theta += deltaTheta;
+
+          Debug("xxxxxxxxxx" << endl);
+      }
+
+      // close the bottom
+      glNormal3d(leftX[i] , y[i], leftZ[i]);
+      glVertex3d(leftX[i] , y[i], leftZ[i]);
+      glNormal3d(rightX[i] , y[i], rightZ[i]);
+      glVertex3d(rightX[i] , y[i], rightZ[i]);
+      glNormal3d(rightX[i+1], -1, rightZ[i+1]);
+      glVertex3d(rightX[i+1], -1, rightZ[i+1]);
+      glNormal3d(leftX[i+1] , -1, leftZ[i+1]);
+      glVertex3d(leftX[i+1] , -1, leftZ[i+1]);
+
+      glEnd();
+
+  }
+
+  delete [] y;
+  delete [] r;
+  delete [] leftX;
+  delete [] leftZ;
+  delete [] rightX;
+  delete [] rightZ;
+}
